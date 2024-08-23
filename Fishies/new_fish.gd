a62628aa8ce3c5ba30fa62f52269b9532431b49a
lukham12 <99699391+lukham12@users.noreply.github.com>
2024-08-23 12:24:31 +1200

    Boids
    I worked on my boids algorithm, added influence by nearby fish velocity

---
+++ Fishies/new_fish.gd
@@ -0,0 +1,13 @@
+extends Node3D
+class_name Fish
+
+@export var velocity : Vector3 = Vector3.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- Scenes/Boids.gd
+++ Scenes/Boids.gd
@@ -1,25 +1,15 @@
 extends Node3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-var fishVelocity = {};
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var fishies = get_children();
 	
 	# Loop through all fish and find total sum of positions
 	var centreOfMassSum = Vector3.ZERO;
+	var sumOfVelocity = Vector3.ZERO;
 	for fish in fishies:
 		centreOfMassSum += fish.global_position;
-	
-	# Loop through all velocities
-	var sumOfVelocity = Vector3.ZERO;
-	for velocity in fishVelocity:
-		sumOfVelocity += velocity;
+		sumOfVelocity += fish.velocity;
 	
 	# Loop through all fish and find local COM
 	for fish in fishies:
@@ -45,19 +35,15 @@ func _process(delta: float) -> void:
 				#target = fish.global_position - (fish.global_position - colliderPosition);
 		
 		# Match velocity of nearby fish
+		var localVelocity = sumOfVelocity - fish.velocity;
+		var localAverageVelocity = localVelocity / (fishies.size() - 1);
 		
-		if not fishVelocity[fish.get_instance_id()]:
-			fishVelocity[fish.get_instance_id()] = Vector3.ZERO;
-		
-		var localVelocity = sumOfVelocity - fishVelocity[fish.get_instance_id()];
-		var localAverageVelocity = localVelocity - (fishies.size() - 1);
-		
-		netVelocity += (localAverageVelocity - fishVelocity[fish.get_instance_id()]) / 8;
+		netVelocity += (localAverageVelocity - fish.velocity) / 8;
 		
 		# Move towards target
 		var target = fish.global_position + netVelocity;
 		
-		fishVelocity[fish.get_instance_id()] = netVelocity;
+		fish.velocity = netVelocity;
 		
 		fish.look_at(target);
 		fish.global_position = target;
